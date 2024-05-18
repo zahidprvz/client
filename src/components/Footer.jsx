129f548e89e8e11218226e47f0478be1e28a37a4
zahidprvz <pervaizzahid55@gmail.com>
2024-05-18 17:46:57 +0500

    decommiting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
